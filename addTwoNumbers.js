221aa5b72a941285bb9c6284b924506ac06f3888
Brennan Caldwell <brennan.caldwell@gmail.com>
2020-09-21 19:54:13 -0700

    Added comment w/ more elegant solution

--- addTwoNumbers.js
+++ addTwoNumbers.js
@@ -102,4 +102,27 @@ function add(str1, str2) {
 Believe this has to do with JS not being able to add very large numbers.
 
 Added the final section, which deals with the addition, and all tests are passing.
+
+
+Another solution:
+
+var addTwoNumbers = function(l1, l2) {
+    let node = null
+    const carry = arguments[2]
+    if (l1 || l2) {
+        const val1 = l1 ? l1.val : 0
+        const val2 = l2 ? l2.val : 0
+        const next1 = l1 ? l1.next : null
+        const next2 = l2 ? l2.next : null
+        const val = carry ? val1 + val2 + 1 : val1 + val2
+        node = new ListNode(val % 10)
+        node.next = addTwoNumbers(next1, next2, val >= 10)
+    } else if (carry) {
+        node = new ListNode(1)
+        node.next = null
+    }
+    return node
+};
+
+What I love about this solution is it handles the adding quirk *as* it is adding *as* it is converting to linked list.
 */
