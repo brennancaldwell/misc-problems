9de8bb4f9478d3577cebf3858b0b97d185c87b72
Brennan Caldwell <brennan.caldwell@gmail.com>
2020-10-02 15:12:42 -0400

    Add comment w/ Floyd's algorithm

--- findDuplicate.js
+++ findDuplicate.js
@@ -27,4 +27,28 @@ var findDuplicate = function(nums) {
 /*
 Time complexity: O(n)
 Space complexity: O(n)
+
+To achieve O(1) space complexity, use Floyd's algorithm, which reduces the
+problem to a Linked List, leaning into the fact that we can use the array
+number values as indices. Two pointers, a tortoise and a hare, are first set
+to discover an intersection point within the cycle created by a duplicate
+value. Then, reduce the hare's "speed" to match that of the tortoise, and
+eventually the two will intersect at the duplicate value.
+
+const findDup = (nums) => {
+  let tortoise = nums[0];
+  let hare = nums[0];
+  do {
+    tortoise = nums[tortoise];
+    hare = nums[nums[hare]];
+  } while (tortoise !== hare);
+
+  tortoise = nums[0];
+  while (tortoise !== hare) {
+    tortoise = nums[tortoise];
+    hare = nums[hare];
+  }
+
+  return hare;
+}
 */
