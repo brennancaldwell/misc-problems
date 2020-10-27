355a3179c0d7850f10c35a8e7fb5d438badc3810
Brennan Caldwell <brennan.caldwell@gmail.com>
2020-10-27 15:10:26 -0400

    Implement conflictingAppointments

---
+++ grokking/mergeIntervals/conflictingAppointments.js
@@ -0,0 +1,20 @@
+/*
+Grokking: https://www.educative.io/courses/grokking-the-coding-interview/qVV79nGVgAG
+
+Given an array of intervals representing ‘N’ appointments, find out if a
+person can attend all the appointments.
+*/
+
+const canAttendAllAppointments = (intervals) => {
+  intervals.sort((a, b) => a.start - b.start);
+  for (let i = 1; i < intervals.length; i++) {
+    if (overlap(intervals[i - 1], intervals[i])) {
+      return false;
+    }
+  }
+  return true;
+}
+
+const overlap = (intervalA, intervalB) => {
+  return intervalA.end <= intervalB.start;
+}
