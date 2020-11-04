9949313ea5779117f6dfeddfaa74007d67299965
Brennan Caldwell <brennan.caldwell@gmail.com>
2020-11-04 13:31:38 -0500

    Add comment with solution code

--- grokking/linkedListReverse/rotateLinkedList.js
+++ grokking/linkedListReverse/rotateLinkedList.js
@@ -7,6 +7,7 @@ LinkedList to the right by ‘k’ nodes.
 
 function rotate(head, rotations) {
   if (rotations <= 0 || head = null || head.next === null) return head;
+
   let i = 0, current = head, oldHead = head;
 
   while (i < rotations) {
@@ -27,3 +28,33 @@ function rotate(head, rotations) {
 
   return head;
 }
+
+/*
+function rotate(head, rotations) {
+  if (head === null || head.next === null || rotations <= 0) {
+    return head;
+  }
+
+  let lastNode = head;
+  let listLength = 1;
+
+  while (lastNode.next !== null) {
+    lastNode = lastNode.next;
+    listLength++;
+  }
+
+  lastNode.next = head;
+  rotations %= listLength;
+  let skipLength = listLength - rotations;
+  let lastNodeOfRotatedList = head;
+
+  for (let i = 0; i < skipLength - 1; i++) {
+    lastNodeOfRotatedList = lastNodeOfRotatedList.next;
+  }
+
+  head = lastNodeOfRotatedList.next;
+  lastNodeOfRotatedList.next = null;
+
+  return head;
+}
+*/
