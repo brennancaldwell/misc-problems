1dbebffaee7accf17d9938ee3e4b9817dcdece30
Brennan Caldwell <brennan.caldwell@gmail.com>
2020-11-10 12:31:04 -0500

    Improve randomizedSet with new implementation for O(1) random access

--- randomizedSet.js
+++ randomizedSet.js
@@ -15,6 +15,58 @@ Follow up: Could you implement the functions of the class with each
 function works in average O(1) time?
 */
 
+class RandomizedSet {
+  constructor() {
+    this.storage = new Map();
+    this.set = [];
+  }
+
+  insert(val) {
+    if (this.storage.has(val)) return false;
+    this.storage.set(val, this.set.length);
+    this.set.push(val);
+    return true;
+  }
+
+  remove(val) {
+    if (!this.storage.has(val)) return false;
+    const index = this.storage.get(val);
+    const last = this.set[this.set.length - 1];
+    this.set[index] = last;
+    this.storage.set(last, index);
+    this.set.pop();
+    this.storage.delete(val);
+    return true;
+  }
+
+  getRandom() {
+    return this.set[Math.floor(Math.random() * this.set.length)];
+  }
+}
+
+
+/*
+Below is my naive implementation of RandomizedSet
+Insert and removal is O(1) and random access is O(n)
+Size is O(n)
+Runtime 440ms (faster than 12.44%)
+Memory 48.8MB (less than 6.14%)
+
+Major drawback is obviously random access, which is a problem with my data
+structure: objects have O(1) lookup for named keys, but we cannot randomly
+choose one by number -- that can be accomplished by an array.
+
+Above, I implement a solution I found online that solves this problem by
+using *two* data structures: 1) a Map, which stores each value along with
+its corresponding index in 2) an array. This way, we have the benefit of
+O(1) insertion and removal and O(1) random access with the drawback of
+doubling our storage. Since time is less expensive than space, this might
+be the best optimization.
+
+Runtime 160ms (faster than 54.44%)
+Memory 47.3MB (less than 6.14%)
+
+
 const RandomizedSet = function() {
   this.storage = {};
   this.size = 0;
@@ -37,3 +89,5 @@ RandomizedSet.prototype.remove = function(val) {
 RandomizedSet.prototype.getRandom = function() {
   return Object.keys(this.storage)[Math.floor(Math.random() * this.size)];
 }
+
+*/
