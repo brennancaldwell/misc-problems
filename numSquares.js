ab3ab44560356c1d004f857460e3009716de6b1b
Brennan Caldwell <brennan.caldwell@gmail.com>
2020-11-13 12:59:11 -0500

    Implement dp approach to numSquares, with explanatory comments

--- numSquares.js
+++ numSquares.js
@@ -5,6 +5,29 @@ Given a positive integer n, find the least number of perfect square numbers
 (for example, 1, 4, 9, 16, ...) which sum to n.
 */
 
+function numSquares(n) {
+  // Initialize an array to track minimum number of squares added to reach
+  // current index. Each *index* represents a number we hit on the way to
+  // n, where as the array index item represents the minimum number of
+  // squares it took us to get there.
+  const dp = new Array(n + 1);
+  dp[0] = 0;
+
+  // Increment values up to n
+  for (let num = 1; num <= n; num++) {
+    dp[num] = Infinity;
+    for (let sq = 1; sq * sq <= num; sq++) {
+      // Array index value will be the minimum number of squares it has
+      // taken us so far to reach our current num value. For instance,
+      // when sq is 2 and num is 4, our minimum value will be
+      // dp[4 - (2 * 2)] + 1 = dp[0] + 1 = 1
+      dp[num] = Math.min(dp[num], dp[num - (sq * sq)] + 1);
+    }
+  }
+
+  return dp[n];
+}
+
 /*
 Naive solution timing out:
 var numSquares = function(n) {
