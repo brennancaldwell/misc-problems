3fb4a38769f25863a7de9d1cd27ed27859dfb160
Brennan Caldwell <brennan.caldwell@gmail.com>
2020-11-16 14:48:53 -0500

    Implement findSumOfPathNumbers

---
+++ grokking/depthFirstSearch/findSumOfPath.js
@@ -0,0 +1,14 @@
+/*
+Grokking: https://www.educative.io/courses/grokking-the-coding-interview/YQ5o5vEXP69
+
+Given a binary tree where each node can only have a digit (0-9) value, each
+root-to-leaf path will represent a number. Find the total sum of all the
+numbers represented by all paths.
+*/
+
+function findSumOfPathNumbers(root, sum = 0) {
+  if (!root) return 0;
+  sum += root.value;
+  if (!root.left && !root.right) return sum;
+  return findSumOfPathNumbers(root.left, sum * 10) + findSumOfPathNumbers(root.right, sum * 10);
+}
