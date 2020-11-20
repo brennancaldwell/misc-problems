6264eb6f53ced5fce1237910e79899fb240c2639
Brennan Caldwell <brennan.caldwell@gmail.com>
2020-11-20 11:26:52 -0500

    Implement findMedianOfStream

---
+++ grokking/twoHeaps/findMedianNumberStream.js
@@ -0,0 +1,42 @@
+/*
+Grokking: https://www.educative.io/courses/grokking-the-coding-interview/3Yj2BmpyEy4
+
+Design a class to calculate the median of a number stream. The class should
+have the following two methods:
+
+1. insertNum(int num): stores the number in the class
+2. findMedian(): returns the median of all numbers inserted in the class
+If the count of numbers inserted in the class is even, the median will be
+the average of the middle two numbers.
+*/
+
+const Heap = require('collections/heap');
+
+class MedianOfStream {
+  constructor() {
+    this.maxHeap = new Heap([], null, (a, b) => a - b);
+    this.minHeap = new Heap([], null, (a, b) => b - a);
+  }
+
+  insertNum(num) {
+    if (this.maxHeap.length === 0 || num <= this.maxHeap.peak()) {
+      this.maxHeap.push(num);
+    } else {
+      this.minHeap.push(num);
+    }
+
+    if (this.maxHeap.length > this.minHeap.length + 1) {
+      this.minHeap.push(this.maxHeap.pop());
+    } else if (this.maxHeap.length < this.minHeap.length) {
+      this.maxHeap.push(this.minHeap.pop());
+    }
+  }
+
+  findMedian() {
+    if (this.maxHeap.length === this.minHeap.length) {
+      return (this.minHeap.peek() + this.maxHeap.peek()) / 2
+    }
+    return this.maxHeap.peek();
+  }
+};
+
