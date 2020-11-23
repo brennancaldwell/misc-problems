598f8851cb109b728bb331d5ab91457702766bb6
Brennan Caldwell <brennan.caldwell@gmail.com>
2020-11-23 15:05:35 -0500

    Add test cases for findMaximumCapital

--- grokking/twoHeaps/maximizeCapital.js
+++ grokking/twoHeaps/maximizeCapital.js
@@ -55,3 +55,8 @@ function findMaximumCapital(capital, profits, numberOfProjects, initialCapital) 
   return availableCapital;
 };
 
+console.log(`Maximum capital: ${findMaximumCapital([0, 1, 2], [1, 2, 3], 2, 1)}`);
+console.log(`Maximum capital: ${findMaximumCapital([0, 1, 2, 3], [1, 2, 3, 5], 3, 0)}`);
+
+
+
